c1b1ff306c7ac2030b8150889e5d3487b10d0a31
Randy <randy.mcbride@centrallogic.com>
2020-07-28 13:28:42 -0600

    first commit

---
+++ src/main/java/com/randymcbride/conferencedemo/models/Speaker.java
@@ -0,0 +1,106 @@
+package com.randymcbride.conferencedemo.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity(name="speakers")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Speaker {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="speaker_id")
+    private Long speakerId;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    private String title;
+
+    private String company;
+
+    @Column(name="speaker_bio")
+    private String speakerBio;
+
+    @ManyToMany(mappedBy = "speakers")
+    @JsonIgnore
+    private List<Session> sessions;
+
+    @Lob
+    @Type(type="org.hibernate.type.BinaryType")
+    @Column(name="speaker_photo")
+    private byte[] speakerPhoto;
+
+    public Speaker() {
+    }
+
+    public Long getSpeakerId() {
+        return speakerId;
+    }
+
+    public void setSpeakerId(Long speakerId) {
+        this.speakerId = speakerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getSpeakerBio() {
+        return speakerBio;
+    }
+
+    public void setSpeakerBio(String speakerBio) {
+        this.speakerBio = speakerBio;
+    }
+
+    public List<Session> getSessions() {
+        return sessions;
+    }
+
+    public void setSessions(List<Session> sessions) {
+        this.sessions = sessions;
+    }
+
+    public byte[] getSpeakerPhoto() {
+        return speakerPhoto;
+    }
+
+    public void setSpeakerPhoto(byte[] speakerPhoto) {
+        this.speakerPhoto = speakerPhoto;
+    }
+}
